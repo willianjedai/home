612204638d2085fc3e53fee7359989095321a9d8
Willian R. Sabião <willianjedai@gmail.com>
2015-02-04 22:12:34 -0200

    Add config files

--- utils.js
+++ utils.js
@@ -1,4 +1,19 @@
-module.exports = {
-  url: '127.0.0.1',
-  port: process.env.PORT
-}
+_extend = require('extend');
+config = require('./config/base.json');
+
+module.exports = function() {
+  // Verify if config-{enviroment}.json file exists and extend config.json with this
+  var configFile = './config/config-'+env+'.json';
+
+  if (env !== 'production' && _fs.existsSync(configFile)) {
+    _extend( true, config, require(configFile) )
+  } else if (env === 'development') {
+    console.log(env)
+    return throw new Error('Por favor, crie o arquivo config-development.json com as configurações locais');
+  }
+  
+  return {
+    url: config.url || '0.0.0.0',
+    port: config.port || process.env.PORT
+  }
+}();
